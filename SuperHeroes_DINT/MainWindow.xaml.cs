b73e072d56018547ee9fda734ae16204bfe5803d
Joan Seva Bernabeu <joan.seva@iesdoctorbalmis.com>
2020-11-30 18:22:50 +0100

    Hago cosas

--- SuperHeroes_DINT/MainWindow.xaml.cs
+++ SuperHeroes_DINT/MainWindow.xaml.cs
@@ -20,9 +20,90 @@ namespace SuperHeroes_DINT
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Superheroe> superheroes;
+        int numero;
         public MainWindow()
         {
             InitializeComponent();
+            numero = 0;
+            superheroes = Superheroe.GetSamples();
+            Actualiza(superheroes[numero]);
         }
+        private void retrocederheroe_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            if(numero > 1)
+            {
+                numero--;
+                Actualiza(superheroes[numero]);
+            }
+        }
+        private void avanzarheroe_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        {
+            if(numero < superheroes.Count - 1)
+            {
+                numero++;
+                Actualiza(superheroes[numero]);
+            }
+        }
+        private void Actualiza(Superheroe aux)
+        {
+
+            verSuperHeroe.DataContext = aux;
+            contadorTextBlock.Text = (numero + 1).ToString() + "/" + superheroes.Count.ToString();
+
+            if (aux.Heroe)
+            {
+                verSuperHeroe.Background = Brushes.PaleGreen;
+                if (aux.Vengador) avengersImage.Visibility = Visibility.Visible;
+                if (aux.Xmen) xMenImage.Visibility = Visibility.Visible;
+            }
+            else verSuperHeroe.Background = Brushes.IndianRed;
+
+        }
+        private void aceptarButton_Click(object sender,RoutedEventArgs e)
+        {
+            string nombre = nombreTextBox.Text;
+            string imagen = imagenTextBox.Text;
+            bool vengador;
+            bool xmen;
+            bool heroe;
+            bool villano;
+            if (heroeRadioButton.IsChecked == false)
+            {
+                heroe = false;
+                vengador = false;
+                xmen = false;
+                villano = true;
+            }
+            else
+            {
+                heroe = true;
+                villano = false;
+
+                if (vengadoresCheckbox.IsChecked == true) vengador = true;
+                else vengador = false;
+
+                if (xMenCheckBox.IsChecked == true) xmen = true;
+                else xmen = false;
+            }
+            Superheroe aux = new Superheroe(nombre,imagen,vengador,xmen,heroe,villano);
+            superheroes.Add(aux);
+            MessageBox.Show("Superheroe insertado con exito");
+            Limpiar();
+        }
+        private void limpiarButton_Click(object sender,RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+        private void Limpiar()
+        {
+            nombreTextBox.Text = "";
+            imagenTextBox.Text = "";
+            heroeRadioButton.IsChecked = true;
+            villanoRadioButton.IsChecked = false;
+            vengadoresCheckbox.IsChecked = false;
+            xMenCheckBox.IsChecked = false;
+        }
+
     }
 }
---
+++ SuperHeroes_DINT/Superheroe.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroes_DINT
+{
+    class Superheroe:INotifyPropertyChanged
+    {
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        public bool Vengador { get; set; }
+        public bool Xmen { get; set; }
+        public bool Heroe { get; set; }
+        public bool Villano { get; set; }
+
+        public Superheroe()
+        {
+        }
+
+        public Superheroe(string nombre, string imagen, bool vengador, bool xmen, bool heroe, bool villano)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Vengador = vengador;
+            Xmen = xmen;
+            Heroe = heroe;
+            Villano = villano;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public static List<Superheroe> GetSamples()
+        {
+            List<Superheroe> ejemplos = new List<Superheroe>();
+
+            Superheroe ironman = new Superheroe("Ironman", @"https://sm.ign.com/ign_latam/screenshot/default/ybbpqktez5whedr0-1592031889_31aa.jpg", true, false, true, false);
+            Superheroe kingpin = new Superheroe("Kingpin", @"https://www.comicbasics.com/wp-content/uploads/2017/09/Kingpin.jpg", false, false, false, true);
+            Superheroe spiderman = new Superheroe("Spiderman", @"https://wipy.tv/wp-content/uploads/2019/08/destino-de-%E2%80%98Spider-Man%E2%80%99-en-los-Comics.jpg", true, true, true, false);
+
+            ejemplos.Add(ironman);
+            ejemplos.Add(kingpin);
+            ejemplos.Add(spiderman);
+
+            return ejemplos;
+        }
+    }
+}
